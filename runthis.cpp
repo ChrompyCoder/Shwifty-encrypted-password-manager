de1a327a50dd2a3572079d94f3f4da635776e36c
ChrompyCoder <sanjupokemon@gmail.com>
2024-03-15 00:39:29 +0530

    code completed but error in SHA header file

--- runthis.cpp
+++ runthis.cpp
@@ -1,12 +1,15 @@
-#include<iostrem>
+#include<iostream>
 #include<encrypter.cpp>
 #include<decrypter.cpp>
 #include<string.h>
+#include<string>
 #include<fstream>
+#include<hashpp.h>
+using namespace hashpp;
 using namespace std;
 
 int main(){
-	cout<<"Welcome!"<<endl<<"[add new: a, discord: d, facebook: f, github: g, insta: i, mail:m, raddit: r]"<<endl;
+	cout<<"Welcome!"<<endl<<"[add new: a, discord: d, facebook: f, github: g, insta: i, mail:m, reddit: r]"<<endl;
 	fstream passw("lockhead.txt", ios::app | ios::out | ios::in);
 	if (is_empty(file)){
 		first_timer();
@@ -16,5 +19,3 @@ int main(){
 	}
 	return 0;
 }
-
-
